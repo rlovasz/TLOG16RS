4d473f1c72761bf49bbfb32dc66748f3afa4eb33
rlovasz <rlovasz@precognox.com>
2016-09-06 15:42:52 +0200

    Initial commit

---
+++ HelloWorldMaven/src/main/java/com/mycompany/helloworldmaven/HelloWorldMavenConfiguration.java
@@ -0,0 +1,10 @@
+package com.mycompany.helloworldmaven;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloWorldMavenConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
